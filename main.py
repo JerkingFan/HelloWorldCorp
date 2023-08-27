4641d4f6b0acb7f23131f0c5ea1ffc276d717087
Arseny <143322464+JerkingFan@users.noreply.github.com>
2023-08-27 21:55:43 +0300

    Secuirity
    The development of security in the application of checking the correctness of a variable.

--- main.py
+++ main.py
@@ -1,45 +1,58 @@
-import time
+from variables import *
+
+verification_object = Verification()
 
 
-received_word = input()
-right_word = 'Hello, World!'
-summary_index = 0
+def cyclic_values(arr):
+    for i in range(len(arr)):
 
-switch_on = False
-
-def word_verification(first_argument, second_argument, third_argument):
-    '''
-
-    received - Checking the fact that the received_word variable is a string variable
-                    and stores a reference to 'Hello, World!'.
-    return - True : if the received_word variable is a string variable and stores a reference to 'Hello, World!'.
-            False : if the received_word variable is not a string variable and stores a reference to 'Hello, World!'.
-    '''
-    if (type(first_argument) == str) and (type(third_argument) == str):
-        length_received_word = len(first_argument)
-
-
-        for i in range(length_received_word):
-
-            if first_argument[i] == right_word[i]:
-
-                second_argument = second_argument + 1
-
-            assert received_word[length_received_word-1] == '!'
-            assert received_word[length_received_word-7] == ' '
-
-
-        if second_argument == length_received_word:
-
+        if i == index_right_word:
+            true_right_word = arr[index_right_word]
             return True
 
-    return False
+        if i == index_summary_index:
+            true_summary_index = arr[index_summary_index]
+            return True
+
+        if i == index_switch_on:
+            true_switch_on = arr[index_switch_on]
+            return True
+
+cyclic_values(verification_object.__dict__())
+cyclic_values(verification_object.__ver__())
+
+if True:
+
+    received_word = input()
 
 
+    def word_verification(first_argument, second_argument, third_argument):
+        """
 
-if word_verification(received_word, summary_index, right_word) == True:
+        received - Checking the fact that the received_word variable is a string variable and stores a reference to
+        'Hello, World!'. return - True : if the received_word variable is a string variable and stores a reference to
+        'Hello, World!'. False : if the received_word variable is not a string variable and stores a reference to
+        'Hello, World!'.
+        """
+        if (type(first_argument) == str) and (type(third_argument) == str):
+            length_received_word = len(first_argument)
 
-    switch_on = True
+            for i in range(length_received_word):
 
-else:
-    raise Exception('Unfortunately, you provided the wrong object reference. Try again!')
+                if first_argument[i] == third_argument[i]:
+                    second_argument = second_argument + 1
+
+                assert first_argument[length_received_word - 1] == '!'
+                assert first_argument[length_received_word - 7] == ' '
+
+            if second_argument == length_received_word:
+                return True
+
+        return False
+
+
+    if word_verification(received_word, true_summary_index, true_right_word):
+        switch_on = True
+
+    else:
+        raise 'Unfortunately, you provided the wrong object reference. Try again!'
